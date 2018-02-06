0666228fd679e93b774254d1d727eb87a08f37a0
Hugo Bernal <hugonobernal@gmail.com>
2018-02-05 23:21:13 -0500

    Modified the bundle identifier. Added the text of the first story to show as soon as the app starts as well as the button texts with the setTitle method. Added the code for updating the label and buttons with the story 2 and 3 depending on which botton the user pressed.

--- Destini-iOS11-master/Destini/ViewController.swift
+++ Destini-iOS11-master/Destini/ViewController.swift
@@ -42,6 +42,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        storyTextView.text = story1
+        
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
@@ -52,6 +56,19 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
     
         // TODO Step 4: Write an IF-Statement to update the views
+        
+        if sender.tag == 1 {
+            storyTextView.text = story3
+            
+            topButton.setTitle(answer3a, for: .normal)
+            topButton.setTitle(answer3b, for: .normal)
+        }
+        else if sender.tag == 2 {
+            storyTextView.text = story2
+            
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+        }
                 
         // TODO Step 6: Modify the IF-Statement to complete the story
         
